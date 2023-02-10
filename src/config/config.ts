0869847795c38098b2286a1b29a0688326d31b64
Diego <493223447@qq.com>
2023-02-10 20:46:20 +0800

    优化代码

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,19 +1,7 @@
 ﻿import { ConfigHelper, Level, LogHelper } from "yanyu-helper";
 export class Config {
-  // public static readonly client = { name: "cid" }; //取其中的值用作pv的参数
-  // public static readonly token = { name: "token" };//取其中的值用作uv的参数，进一步取userid作为其他参数值 visiter才是记录访问日志，log只是记录日志两者分开
-
-  public static get isDebug() {
-    return !process.env.NODE_ENV || process.env.NODE_ENV === "debug";
-  }
-  public static readonly configurl =
-    "http://config.yumao.tech/api/startconfig/server";
-
   //网络配置
   public static rediscache: any;
-
-  //自定义配置
-  public static appkey = ""; //预留 也许用来作为jwt中加密的sercrt
   public static redisdomain: string;
   public static apiKey: string;
   public static apiSecret: string;
@@ -21,9 +9,9 @@ export class Config {
   public static aliddnsapi: string;
 
   //基础配置 这些默认值都是无效的只是给看一下具体的类型
-  public static loglevel: Level | undefined = Level.INFO;
-  public static jwtoption = { expiresIn: "7d", secret: "", authname: "token" };
-  public static clientidname = "cid";
+  public static loglevel: Level;
+  public static jwtoption: any;
+  public static clientidname: string;
   public static openpms: boolean;
   public static logurl: string;
   public static pmsurl: string;
